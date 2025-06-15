a1aa030a3d045305332720d885d6d1a49b230f6f
dynilath <dynilath@qq.com>
2025-06-15 12:56:54 +0800

    refactor: simplify Leaflet bundle imports and remove unused exports

--- bundle/leaflet-bundle.js
+++ bundle/leaflet-bundle.js
@@ -1,13 +1,11 @@
-// Leaflet Bundle - 包含 Leaflet 及其相关插件
-import * as L from "leaflet";
+import "leaflet";
 import "leaflet-contextmenu";
 import "leaflet-easybutton";
+
+L.easyButton = L.easyButton;
+L.easyBar = L.easyBar;
 
 // 将 Leaflet 暴露到全局
 if (typeof window !== "undefined") {
   window.L = L;
 }
-
-// 导出所有内容
-export default L;
-export * from "leaflet";
